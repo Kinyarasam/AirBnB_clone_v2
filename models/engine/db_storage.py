5538682ab8c94db1923066c39eb13d98323da932
kinyarasam <samkinyara2000@gmail.com>
2022-09-14 22:24:03 +0300

    Bug Free. Console improvements. (def do_create) function of the command interpreter to allow for Object creation with given parameters. MySQL setup development.

---
+++ models/engine/db_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Database storage engine"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.amenity import Amenity
+from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from os import getenv
+
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.place import place_amenity
+
+classes = {
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
+}
+
+
+class DBStorage:
+    """Database storage engine for mysql storage
