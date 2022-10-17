4161ad335d06d203ad70fa96d64bda9b0cd51832
kinyarasam <skinyara.30@gmail.com>
2022-10-17 12:51:57 +0300

    web_flask/

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,12 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+"""Documentation"""
 @app.route('/', strict_slashes=False)
 def hello():
     """Displays the message Hello HBNB!"""
     return 'Hello HBNB!'
 
-
+"""Run server on port 5000"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
